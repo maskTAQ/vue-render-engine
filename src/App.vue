d25c5dcd7ae64d86ffa0488f728cbc0b87deaa73
chengqi77 <2113265828@qq.com>
2020-06-15 14:29:05 +0800

    src

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,49 @@
 <template>
-  <div id="app">
-	<MainLeft></MainLeft>
-    <Engine mode="h5" :dataInject="dataInject" :nodeInject="nodeInject" :bridge="bridge" />
-  </div>
+	<div id="app">
+		<a-row>
+			<a-col :span="8">
+				<MainLeft></MainLeft>
+			</a-col>
+			<a-col :span="8">
+				<Engine mode="h5" :dataInject="dataInject" :nodeInject="nodeInject" :bridge="bridge" />
+			</a-col>
+			<!-- <a-col :span="8">
+				配置
+			</a-col> -->
+		</a-row>
+	</div>
 </template>
 
 <script>
-import Engine from "@/Engine";
-import MainLeft from "@/components/mainLeft";
-import Bridge from "@/Engine/utils/Bridge";
-import { dataInject } from "@/utils";
-import nodeInject from "./components";
+	import Engine from "@/Engine";
+	import MainLeft from "@/components/mainLeft";
+	import Bridge from "@/Engine/utils/Bridge";
+	import {
+		dataInject
+	} from "@/utils";
+	import nodeInject from "./components";
 
-const bridge = new Bridge();
-window.bridge = bridge;
+	const bridge = new Bridge();
+	window.bridge = bridge;
 
-export default {
-  name: "App",
-  data() {
-    return {
-      dataInject,
-      nodeInject,
-      bridge
-    };
-  },
-  components: {
-    Engine,MainLeft
-  },
-  mounted() {
-    //像引擎发送一个 EVENTS.NODE_MOVE 命令
-    //bridge.execute({ type: bridge.command.EVENTS.NODE_MOVE, data: { x: 0, y: 0 } });
-  }
-};
+	export default {
+		name: "App",
+		data() {
+			return {
+				dataInject,
+				nodeInject,
+				bridge
+			};
+		},
+		components: {
+			Engine,
+			MainLeft
+		},
+		mounted() {
+			//像引擎发送一个 EVENTS.NODE_MOVE 命令
+			//bridge.execute({ type: bridge.command.EVENTS.NODE_MOVE, data: { x: 0, y: 0 } });
+		}
+	};
 </script>
 
 <style lang="scss">
--- src/components/mainLeft.vue
+++ src/components/mainLeft.vue
@@ -1,16 +1,15 @@
 <template>
-    <div class="wf-panel wf-widgetspanel">
-        <div class="wf-panel-tab">
+    <div>
+        <div>
             <a href="javascript:;" class="tabitem current">控件</a>
         </div>
-        <div class="wf-panel-body">
+        <div>
             <div v-for="(item,index) in components"
-                 class="wf-widgetsitem" v-bind:data-index="index" v-bind:data-type="item.componentName"
+                 v-bind:data-index="index" v-bind:data-type="item.componentName"
                  v-on:mousedown="start">
                 <label>
                     {{item.name}}
                 </label>
-                <i class="widgeticon" v-bind:class="item.componentName"></i>
             </div>
 
         </div>
@@ -37,25 +36,7 @@
         },
         methods: {
             start: function (e) {
-                let obj = {}
-                let dom = e.currentTarget
-                let index = dom.getAttribute('data-index')
-                let actualLeft = dom.offsetLeft;
-                let current = dom.offsetParent;
-                let actualTop = dom.offsetTop;
-                while (current !== null) {
-                    actualLeft += current.offsetLeft;
-                    actualTop += current.offsetTop;
-                    current = current.offsetParent;
-                }
-                obj.componentName = dom.getAttribute("data-type")
-                obj.componentText = dom.querySelector('label').innerText
-                obj.clientX = e.clientX
-                obj.clientY = e.clientY
-                obj.isstart = true
-                obj.componentView = this.components[index]
-                console.log(obj)
-                drag.$emit("movestart", obj)
+               
             }
         }
     }
